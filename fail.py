000e07822953c6d0b55ba016f9035ca4405149d2
kim-mini <72716397+kim-mini@users.noreply.github.com>
2022-05-20 10:49:18 +0900

    Update fail.py

--- fail.py
+++ fail.py
@@ -3,8 +3,12 @@ def solution(N, stages):
     fail = {}
     all = len(stages)
 
-    for i in range(N):
-        fail[i+1] = stages.count(i+1) / all
-        all -= stages.count(i+1)
+    for i in range(1,N+1):
+        if all != 0:
+            count = stages.count(i)
+            fail[i] = count / all
+            all -= count
+        else:
+            fail[i] = 0
 
     return sorted(fail, key=lambda x:fail[x],reverse=True)
