af2a98620a66400f9b8b4e1bb37f31958c93e5d2
kim-mini <72716397+kim-mini@users.noreply.github.com>
2022-05-30 08:59:22 +0900

    Update catchDoll.py

--- catchDoll.py
+++ catchDoll.py
@@ -21,10 +21,3 @@ def solution(board, moves):
                 dst = 0
                 break
     return answer
-
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
-
-print(solution(board, moves))
-
-
